5cad4b554008e4f49fe8cb3d94997c194eda0ac2
Jeremy Lee <145268409+jeremlee@users.noreply.github.com>
2024-03-28 13:43:09 +0800

    Add files via upload

---
+++ heap/heap.h
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cstdlib>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<stdexcept>
+using namespace std;
+
+template<typename T>
+
+class Heap{
+	vector<T> arr;
+	void bubbleUp(int idx){
+		if(idx <= 0){
+			return;
+		}
+		int parentIdx = floor((idx-1)/2.0);
+		if(arr[idx] < arr[parentIdx]){
+			swap(arr[idx],arr[parentIdx]);
+		}
+		bubbleUp(parentIdx);
+	}
+	void bubbleDown(int idx){
+		if(idx >= arr.size()){
+			return;
+		}
+		int left = 2*idx+1;
+		int right = 2*idx+2;
+		int chosenChild = idx;
+		if(left < arr.size() && arr[left] < arr[chosenChild]){
+			chosenChild = left;
+		}
+		if(right < arr.size() && arr[right] < arr[chosenChild]){
+			chosenChild = right;
+		}
+		if(chosenChild == idx){
+			return;
+		} else{
+			swap(arr[idx],arr[chosenChild]);
+			bubbleDown(chosenChild);
+		}
+	}
+public:
+	void insert(T elem){
+		arr.push_back(elem);
+		bubbleUp(arr.size()-1);
+	}
+	T remove(){
+		if(arr.empty()){
+			throw logic_error("nothing because heap is empty!");
+		}
+		T toReturn = arr[0];
+		swap(arr[0],arr[arr.size()-1]);
+		arr.pop_back();
+		bubbleDown(0);
+		return toReturn;		
+	}
+	void display(){
+		if(arr.empty()){
+			throw logic_error("Heap is empty!");
+		}
+		for(T t : arr){
+			cout << t << " ";
+		}
+		cout << endl;
+	}
+};
---
+++ heap/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include"heap.h"
+using namespace std;
+
+int main(void){
+	Heap<int>* heap = new Heap<int>();
+	char op;
+	int num;
+	while(true){
+		cout << "Enter operation: ";
+		cin >> op;
+		try{
+			switch(op){
+			case 'a':
+				cin >> num;
+				heap->insert(num);
+				break;
+			case 'r':
+				cout << "Removed: " << heap->remove() << endl;
+				break;
+			case 'd':
+				heap->display();
+				break;
+			}
+			if(op == 'x'){
+				break;
+			}
+		}catch(logic_error& e){
+			cout << e.what() << endl;
+		}
+	}
+	return 0;
+}
