f527083466610c44c85b10f9eede7721fbfc8dd6
Jeremy Lee <145268409+jeremlee@users.noreply.github.com>
2024-03-29 20:43:53 +0800

    Add files via upload

---
+++ hashtable/separate chaining/LinkedList.h
@@ -0,0 +1,173 @@
+#include<iostream>
+#include<stdexcept>
+#include<cstdlib>
+#include"Node.h"
+using namespace std;
+
+template<typename T, typename Y>
+
+class LinkedList{
+	int size;
+	Node<T,Y>* head;
+	Node<T,Y>* tail;
+public:
+	LinkedList(){
+		size = 0;
+		head = nullptr;
+		tail = nullptr;
+	}
+	void add(T key,Y value){
+		Node<T,Y>* node = new Node<T,Y>(key,value);
+		if(!head){
+			head = node;
+		} else{
+			tail->next = node;
+		}
+		tail = node;
+		tail->next = nullptr;
+		size++;
+		return;
+	}
+	
+	void removeAt(int position){
+		if(!head || position<=0){
+			cout << "No" << endl;
+			return;
+		}
+		if(position == 1){
+			removeHead();
+			return;
+		}
+		if(position == size){
+			removeHead();
+			return;
+		}
+		Node<T,Y>* current = head;
+		int count = 1;
+		while(current && count < position - 1){
+			current = current->next;
+			count++;
+		}
+		Node<T,Y>* temp = current->next;
+		current->next = temp->next;
+		if(!temp->next){
+			current->next = nullptr;
+			tail = current;
+		}
+		delete temp;
+		size--;
+	}
+	
+	void addAt(T key, Y value, int position){
+		if(!head || position<=0){
+			cout << "No" << endl;
+			return;
+		}
+		if(position == 1){
+			addHead(key,value);
+			return;
+		}
+		Node<T,Y>* current = head;
+		int count = 1;
+		while(current && count < position-1){
+			current = current->next;
+			count++;
+		}
+		Node<T,Y>* node = new Node<T,Y>(key,value,current->next);
+		if(!current->next){
+			node->next = nullptr;
+			tail = node;
+		}
+		current->next = node;
+		size++;
+	}
+	
+	void removeHead(){
+		if(!head){
+			return;
+		}
+		if(size == 1){
+			size--;
+			delete head;
+			head = nullptr;
+			tail = nullptr;
+			return;
+		}
+		Node<T,Y>* temp = head;
+		head = head->next;
+		size--;
+		delete temp;
+	}
+	
+	void remove(){
+		if(!head){
+			return;
+		}
+		if(size == 1){
+			size--;
+			delete head;
+			head = nullptr;
+			tail = nullptr;
+			return;
+		}
+		Node<T,Y>* temp = head;
+		while(temp->next != tail){
+			temp = temp->next;
+		}
+		delete tail;
+		temp->next = nullptr;
+		tail = temp;
+		size--;
+	}
+	
+	void removeNode(T key){
+		if(!head){
+			return;
+		}
+		bool delay = false;
+		bool found = false;
+		Node<T,Y>* current = head;
+		Node<T,Y>* delayed = head;
+		while(current){
+			if(current->key == key){
+				found = true;
+				break;
+			}
+			current = current->next;
+			if(!delay){
+				delay = true;
+			} else{
+				delayed = delayed->next;
+			}
+		}
+		if(!found){
+			throw logic_error("Key not found!");
+		}
+		if(current == head){
+			removeHead();
+			return;
+		}
+		if(!current->next){
+			remove();
+			return;
+		}
+		delayed->next = current->next;
+		delete current;
+		size--;
+	}
+
+	void addHead(T key,Y value){
+		Node<T,Y>* node = new Node<T,Y>(key,value,head);
+		head = node;
+		size++;
+	}
+	
+	Node<T,Y>* getHead(){
+		if(!head){
+			return nullptr;
+		}
+		return head;
+	}
+	
+	
+};
---
+++ hashtable/separate chaining/hashtable.h
@@ -0,0 +1,145 @@
+#include<iostream>
+#include<cstdlib>
+#include<functional>
+#include<stack>
+#include<stdexcept>
+#include"LinkedList.h"
+using namespace std;
+
+template<typename T,typename Y>
+
+class HashTable{
+	LinkedList<T,Y>** list;
+	int capacity, size;
+	function<int(const T&,int)> hash;
+	void initialize(){
+		for(int i=0;i<capacity;i++){
+			list[i] = new LinkedList<T,Y>();
+		}
+	}
+	int findNextPrime(int n){
+		int prime = n+1;
+		while(!isPrime(prime)){
+			prime++;
+		}
+		return prime;
+	}
+	bool isPrime(int n) {
+		if(n == 0||n == 1){
+		    return false;
+		}
+		for(int i=2;i<=n/2;i++){
+			if(n%i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	stack<Node<T,Y>*> getAll(){
+		stack<Node<T,Y>*> elements;
+		for(int i=0;i<capacity;i++){
+			if(!list[i]->getHead()){
+				continue;
+			}
+			Node<T,Y>* current = list[i]->getHead();
+			while(current){
+				elements.push(current);
+				current = current->next;
+			}
+		}
+		return elements;
+	}
+	void grow(){
+		stack<Node<T,Y>*> elements = getAll();
+		capacity = findNextPrime(capacity);
+		reHash(elements);
+	}
+	void shrink(){
+		stack<Node<T,Y>*> elements = getAll();
+		capacity = findNextPrime(size-1);
+		if(capacity < 5){
+			capacity = 5;
+		}
+		reHash(elements);
+	}
+	void reHash(stack<Node<T,Y>*> elements){ 
+		delete[] list;
+		list = new LinkedList<T,Y>*[capacity];
+		initialize();
+		Node<T,Y>* temp;
+		while(!elements.empty()){
+			temp = elements.top();
+			int idx = hash(temp->key,capacity);
+			list[idx]->add(temp->key,temp->value);
+			elements.pop();
+		}
+	}	
+	
+	bool checkDuplicate(T key, Y value, int idx){
+		Node<T,Y>* current = list[idx]->getHead();
+		while(current){
+			if(current->key == key){
+				current->value = value;
+				return true;
+			}
+			current = current->next;
+		}
+		return false;
+	}
+public:
+	HashTable(function<int(const T&,int)> customHash){
+		hash = customHash;
+		capacity = 5;
+		size = 0;
+		list = new LinkedList<T,Y>*[capacity];
+		initialize();
+	}
+	
+	void add(T key, Y value){
+		if(size == capacity){
+			grow();
+		}
+		int idx = hash(key,capacity);
+		if(!checkDuplicate(key,value,idx)){
+			list[idx]->add(key,value);
+			size++;
+		}
+	}
+	
+	Y getElement(T key){
+		int idx = hash(key,capacity);
+		Node<T,Y>* current = list[idx]->getHead();
+		while(current){
+			if(current->key == key){
+				return current->value;
+			}
+			current = current->next;
+		}
+		throw logic_error("not found!");
+	}
+	
+	void remove(T key){
+		int idx = hash(key,capacity);
+		list[idx]->removeNode(key);
+		size--;
+		if(size <= capacity*0.75){
+			shrink();
+		}
+	}
+	
+	void display(){
+		cout << "Size: " << size << " Capacity: " << capacity << endl;
+		for(int i=0;i<capacity;i++){
+			if(!list[i]){
+				cout << "Nothing inside" << endl;
+				continue;
+			}
+			Node<T,Y>* current = list[i]->getHead();
+			while(current){
+				cout << "Index: " << i << " Key: " << current->key << " | Value: " << current->value << endl;
+				current = current->next;
+			}
+		}
+	}
+	
+};
